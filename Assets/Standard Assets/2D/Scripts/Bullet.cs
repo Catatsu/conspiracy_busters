559ee1affd5f32d47ef1d36310dae54c34d99b5b
asada_shumei <asada_shumei@hasigo.co.jp>
2016-09-17 17:49:21 +0900

    誘導弾に制限をつける(未完成)

--- Assets/Standard Assets/2D/Scripts/Bullet.cs
+++ Assets/Standard Assets/2D/Scripts/Bullet.cs
@@ -12,6 +12,8 @@ public class Bullet : MonoBehaviour {
     public float target_y;
     //弾の速度
     public float speed;
+    //自機狙い弾の制限角度
+    public float theta;
 
     //playerｵﾌﾞｼﾞｪｸﾄの取得
     public GameObject obj;
@@ -27,7 +29,8 @@ public class Bullet : MonoBehaviour {
     //初期化ﾌﾗｸﾞ(true:初期 false:2回目以降)
     bool farst;
 
-    Vector2 direction;
+    Vector2 direction; //移動方向
+    Vector2 direction_old; //1ﾌﾚｰﾑ前の移動方向
 
     // Use this for initialization
     void Start () {
@@ -46,16 +49,77 @@ public class Bullet : MonoBehaviour {
         switch (type)
         {
             case (int)Bullet_type.target_homing:
-                //playerの位置を代入
-                target_x = obj.transform.position.x;
-                target_y = obj.transform.position.y;
-                //弾の位置を代入
-                bullet_x = transform.position.x;
-                bullet_y = transform.position.y;
-                //移動する向きを求める
-                direction = new Vector2(target_x - bullet_x, target_y - bullet_y).normalized;
-                //移動する向きとスピードを代入する
-                rigidbody2d.velocity = direction * speed;
+
+                //2回以降の場合、上限角度内で移動をさせるために、過去の移動方向を参照して動かす
+                if (farst == false)
+                {
+                    //移動方向を保存
+                    direction_old = direction;
+
+                    //playerの位置を代入
+                    target_x = obj.transform.position.x;
+                    target_y = obj.transform.position.y;
+                    //弾の位置を代入
+                    bullet_x = transform.position.x;
+                    bullet_y = transform.position.y;
+                    //移動する向きを求める
+                    direction = new Vector2(target_x - bullet_x, target_y - bullet_y).normalized;
+
+                    //右回り旋回角度上限のﾍﾞｸﾄﾙを求める
+                    float rad = Mathf.PI / 180 * theta;
+                    float x2 = Mathf.Cos(rad) * direction_old.x - Mathf.Sin(rad) * direction_old.y;
+                    float y2 = Mathf.Sin(rad) * direction_old.x - Mathf.Cos(rad) * direction_old.y;
+
+                    //自機方向と旋回角度上限のどちらに曲がるかを決める
+                    if (direction_old.x * direction.x + direction_old.y * direction.y >= direction_old.x * x2 + direction_old.y * y2)
+                    {
+                        //自機方向が旋回可能範囲内の場合、自機方向に曲がるため、追加処理をしない?
+                    }
+                    else
+                    {
+                        //自機方向が旋回可能範囲外の場合
+                        //左回り旋回角度上限の速度ﾍﾞｸﾄﾙを求める
+                        float x3 = Mathf.Cos(rad) * direction_old.x + Mathf.Sin(rad) * direction_old.y;
+                        float y3 = Mathf.Sin(rad) * direction_old.x + Mathf.Cos(rad) * direction_old.y;
+
+                        //弾から自機への相対位置ﾍﾞｸﾄﾙ(px,py)を求める
+                        float px = target_x - bullet_x;
+                        float py = target_y - bullet_y;
+
+                        //右回りか左回りかを求める
+                        if (px * x2 + py * y2 >= px * x3 + py * y3)
+                        {
+                            //右回りの場合
+                            direction.Set(x2, y2);
+                        }
+                        else
+                        {
+                            //左回りの場合
+                            direction.Set(x3, y3);
+                        }
+                    }
+                    //移動する向きとスピードを代入する
+                    rigidbody2d.velocity = direction * speed;
+                }
+                else
+                {
+                    //playerの位置を代入
+                    target_x = obj.transform.position.x;
+                    target_y = obj.transform.position.y;
+                    //弾の位置を代入
+                    bullet_x = transform.position.x;
+                    bullet_y = transform.position.y;
+                    //移動する向きを求める
+                    direction = new Vector2(target_x - bullet_x, target_y - bullet_y).normalized;
+                    //移動する向きとスピードを代入する
+                    rigidbody2d.velocity = direction * speed;
+
+                    //初期化ﾌﾗｸﾞを変更
+                    if (farst)
+                    {
+                        farst = false;
+                    }
+                }
                 break;
             case (int)Bullet_type.target_straight:
                 if (farst)
