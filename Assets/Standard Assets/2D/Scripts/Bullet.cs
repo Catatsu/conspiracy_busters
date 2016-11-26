169a1058df4bdbd0913b17c128dc84be4df3c22f
浅田 修明 <B07599@cyberagent.co.jp>
2016-11-26 16:53:29 +0900

    FPS固定、誘導弾に誘導時間制限を追加、ﾗﾝﾀﾞﾑ弾追加

--- Assets/Standard Assets/2D/Scripts/Bullet.cs
+++ Assets/Standard Assets/2D/Scripts/Bullet.cs
@@ -26,8 +26,14 @@ public class Bullet : MonoBehaviour {
     //定数:弾の挙動
     enum Bullet_type {
         target_straight,
-        target_homing
+        target_homing_endless,
+        target_homing_countend,
+        random_straight
     };
+
+    //時限処理用
+    float frameCount = 0;
+    public float limit_frame;
 
     //初期化ﾌﾗｸﾞ(true:初期 false:2回目以降)
     bool farst;
@@ -50,7 +56,7 @@ public class Bullet : MonoBehaviour {
 
         switch (type)
         {
-            case (int)Bullet_type.target_homing:
+            case (int)Bullet_type.target_homing_endless:
 
                 //2回以降の場合、上限角度内で移動をさせるために、過去の移動方向を参照して動かす
                 if (farst == false)
@@ -105,7 +111,7 @@ public class Bullet : MonoBehaviour {
                         }
                     }
                     //移動する向きとスピードを代入する
-                    rigidbody2d.velocity = direction * speed;
+                    rigidbody2d.velocity = direction.normalized * speed;
                 }
                 else
                 {
@@ -127,6 +133,93 @@ public class Bullet : MonoBehaviour {
                     }
                 }
                 break;
+            case (int)Bullet_type.target_homing_countend:
+                //制限時間以降はﾎｰﾐﾝｸﾞさせない
+                if (frameCount <= limit_frame )
+                {
+                    ++frameCount;
+
+                    //2回以降の場合、上限角度内で移動をさせるために、過去の移動方向を参照して動かす
+                    if (farst == false)
+                    {
+                        //移動方向を保存
+                        direction_old = direction;
+
+                        //playerの位置を代入
+                        target_x = obj.transform.position.x;
+                        target_y = obj.transform.position.y;
+                        //弾の位置を代入
+                        bullet_x = transform.position.x;
+                        bullet_y = transform.position.y;
+                        //移動する向きを求める
+                        direction = new Vector2(target_x - bullet_x, target_y - bullet_y).normalized;
+
+                        //右回り旋回角度上限のﾍﾞｸﾄﾙを求める
+                        float rad = Mathf.PI / 180 * theta;
+                        float x2 = Mathf.Cos(rad) * direction_old.x - Mathf.Sin(rad) * direction_old.y;
+                        float y2 = Mathf.Sin(rad) * direction_old.x + Mathf.Cos(rad) * direction_old.y;
+
+                        //自機方向と旋回角度上限のどちらに曲がるかを決める
+                        if (direction_old.x * direction.x + direction_old.y * direction.y >= direction_old.x * x2 + direction_old.y * y2)
+                        {
+                            //自機方向が旋回可能範囲内の場合、自機方向に曲がるため、追加処理をしない?
+                            //debug
+                            debugtext.GetComponent<DebugText>().text = 1;
+                        }
+                        else
+                        {
+                            //debug
+                            debugtext.GetComponent<DebugText>().text = 2;
+                            //自機方向が旋回可能範囲外の場合
+                            //左回り旋回角度上限の速度ﾍﾞｸﾄﾙを求める
+                            float x3 = Mathf.Cos(rad) * direction_old.x + Mathf.Sin(rad) * direction_old.y;
+                            float y3 = -Mathf.Sin(rad) * direction_old.x + Mathf.Cos(rad) * direction_old.y;
+
+                            //弾から自機への相対位置ﾍﾞｸﾄﾙ(px,py)を求める
+                            float px = target_x - bullet_x;
+                            float py = target_y - bullet_y;
+
+                            //右回りか左回りかを求める
+                            if (px * x2 + py * y2 >= px * x3 + py * y3)
+                            {
+                                //右回りの場合
+                                direction.Set(x2, y2);
+                            }
+                            else
+                            {
+                                //左回りの場合
+                                direction.Set(x3, y3);
+                            }
+                        }
+                        //移動する向きとスピードを代入する
+                        rigidbody2d.velocity = direction.normalized * speed;
+                    }
+                    else
+                    {
+                        //playerの位置を代入
+                        target_x = obj.transform.position.x;
+                        target_y = obj.transform.position.y;
+                        //弾の位置を代入
+                        bullet_x = transform.position.x;
+                        bullet_y = transform.position.y;
+                        //移動する向きを求める
+                        direction = new Vector2(target_x - bullet_x, target_y - bullet_y).normalized;
+                        //移動する向きとスピードを代入する
+                        rigidbody2d.velocity = direction * speed;
+
+                        //初期化ﾌﾗｸﾞを変更
+                        if (farst)
+                        {
+                            farst = false;
+                        }
+                    }
+                }
+                else
+                {
+                    //移動する向きとスピードを代入する
+                    rigidbody2d.velocity = direction * speed;
+                }
+                break;
             case (int)Bullet_type.target_straight:
                 if (farst)
                 {
@@ -143,6 +236,17 @@ public class Bullet : MonoBehaviour {
                 direction = new Vector2(target_x - bullet_x, target_y - bullet_y).normalized;
                 //移動する向きとスピードを代入する
                 rigidbody2d.velocity = direction * speed;
+                break;
+            case (int)Bullet_type.random_straight:
+                if (farst)
+                {
+                    //targetにﾗﾝﾀﾞﾑ値を代入
+                    direction = new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+                    //2回目以降実行しない
+                    farst = false;
+                }
+                //移動する向きとスピードを代入する
+                rigidbody2d.velocity = direction.normalized * speed;
                 break;
         }
 
